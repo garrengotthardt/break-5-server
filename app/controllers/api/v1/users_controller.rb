ba3d640c2a6b5fae1e9a21b72a3ea2c254bdde38
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-08-12 13:31:08 -0400

    udpate user address when setting current search location, but still passing in before state is updated

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,10 +19,20 @@ module Api
         end
       end
 
+      def update
+        @user = User.find(params[:id])
+
+        if @user.update(lat: params[:lat], long: params[:long], address: params[:address])
+          render json: {user: @user, message: "User was updated!", status: 201}
+        else
+          render json: @user.errors, message: "Could not update User!", status: 401
+        end
+      end
+
       private
 
       def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address, :lat, :long)
+        params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :address, :lat, :long)
       end
     end
 
