4d8b376f31adb3382cc7706d2dd300d7e18ec5de
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-10-04 13:30:09 -0400

    deleted some unnecessary commented out text

--- app/serializers/user_place_serializer.rb
+++ app/serializers/user_place_serializer.rb
@@ -2,6 +2,4 @@ class UserPlaceSerializer < ActiveModel::Serializer
   attributes :id, :place_id, :user_id
   belongs_to :user
   has_many :places
-  # has_many :item_variations, through: :menu_items, foreign_key: :menu_item_id
 end
-#
